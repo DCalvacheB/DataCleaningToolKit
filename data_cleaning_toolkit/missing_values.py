2e66c1e005530d324bf4a0c4c3e4bfed65e4cf78
Monaa0536 <calvached0536@gmail.com>
2025-03-06 10:29:58 -0500

    Missing values module eval

--- data_cleaning_toolkit/missing_values.py
+++ data_cleaning_toolkit/missing_values.py
@@ -2,14 +2,29 @@ import pandas as pd
 from sklearn import datasets
 import random
 
+
+
 def fill_quant_cols(df, strategy:str, col:str)->pd.DataFrame:
+    """
+    Fill missing values in a quantitative column using a specified strategy.
+
+    Parameters:
+    - df (pd.DataFrame): The input DataFrame.
+    - strategy (str): The strategy to use for filling missing values. Options are "mean", "median", or "mode".
+    - col (str): The column to fill missing values in.
+
+    Returns:
+    - pd.DataFrame: A DataFrame with filled missing values.
+
+    """
     if strategy == "mean":
-        df[col].fillna(df[col].mean(), inplace=True)
+        df.fillna({col: df[col].mean()}, inplace=True)  
     elif strategy == "median":
-        df[col].fillna(df[col].median(), inplace=True)
+        df.fillna({col:df[col].median()}, inplace=True)
     elif strategy == "mode":
-        df[col].fillna(df[col].mode()[0], inplace=True)
+        df.fillna({col:df[col].mode()[0]}, inplace=True)
     return df
+
 def fill_qual_cols(df, strategy:str, col:str)->pd.DataFrame:
     if strategy == "mode":
         df[col].fillna(df[col].mode()[0], inplace=True)
@@ -38,16 +53,44 @@ def fill_missing_values(df, strategy=["mean", 'bfill'], configs:dict=None):
     qual_cols = [col for col in df.columns if df[col].dtype=='object']
 
     for col in df.columns:
-        if configs is not None:
-            strategy = configs.get(col, strategy[0]) if col in quant_cols else configs.get(col, strategy[1])
+        if configs is None:
+            configs = {}
+        try:
+            strategy_qan = configs.get(col, strategy[0]) if col in quant_cols else configs.get(col, strategy[0])
+            strategy_qal = configs.get(col, strategy[1]) if col in quant_cols else configs.get(col, strategy[1])
+        except IndexError:
+            if len(strategy)==1:
+                strategy_qan = configs.get(col, strategy[0]) if col in quant_cols else configs.get(col, strategy[0])
+                strategy_qal = 'bfill'
         if col in quant_cols:
-            df = fill_quant_cols(df=df, strategy=strategy[0], col=col)
+            df = fill_quant_cols(df=df, strategy=strategy_qan, col=col)
         if col in qual_cols:
-            df = fill_qual_cols(df=df, strategy=strategy[1], col=col)
+            df = fill_qual_cols(df=df, strategy=strategy_qal, col=col)
     return df
 
 
+def drop_missing_values(df, axis=0, threshold=0):
+    """
+    Drop rows or columns with missing values.
 
+    Parameters:
+    - df (pd.DataFrame): The input DataFrame.
+    - axis (int): 0 to drop rows, 1 to drop columns.
+    - threshold (int, optional): Minimum number of non-NA values required to keep the row or column. Defaults to None.
+
+    Returns:
+    - pd.DataFrame: A DataFrame with rows or columns dropped.
+    """
+    print('Dropping missing values...')
+    print('df shape before dropping missing values:', df.shape)
+    df1 = df.dropna(axis=axis, thresh=threshold)
+    print('df shape after dropping missing values:', df1.shape)
+    if df.shape[0] == df1.shape[0]:
+        print('No rows dropped.')
+    if df.shape[1] == df1.shape[1]:
+        print('No columns dropped.')
+    
+    return df1
 
 
 def missing_values_summary(df):
